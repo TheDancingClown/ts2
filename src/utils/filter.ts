86947357377acc798134d8b38152ed64984f7fae
Louis Kirkham <chew@talk21.com>
2021-02-15 12:27:53 +0000

    added documentation comment to find_talent method

--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -10,7 +10,14 @@ class TalentFilter {
   constructor(searchParameter = '') {
     this.searchParameter = searchParameter;
   };
-
+/**
+ * Returns an array of talent objects using a searchParameter attribute key as specified in the constructor
+ * 
+ * @param data - an array of all talent objects
+ * @param searchQuery - a string to filter against the attribute value
+ * @returns Returns an array of matching Talent objects
+ * 
+ */
   find_talent = (data: Talent[], searchQuery: string):Talent[] => {
     return data.filter(talent => talent[this.searchParameter].toLowerCase() === searchQuery.toLowerCase());
   };
